1570fac867b777ef11b4234bbeaa6c41c7156131
Leo4815162342 <leonid.pyrlya@gmail.com>
2019-05-21 21:56:15 +0200

    remove get-endpoint-type fn

--- src/request-generator/index.ts
+++ src/request-generator/index.ts
@@ -1,15 +1,24 @@
 import { HistoryConfig } from '../config/types';
-import { RequestObject, EndopointToFilePeriodMap } from './types';
+import { RequestObject, EndopointToFilePeriodMap, EndpointType } from './types';
 import { getStarOfUtc } from './../utils';
 
 import getUrl from './get-url';
-import getEndpointType from './get-endpoint-type';
 
 const endopointToFilePeriodMap: EndopointToFilePeriodMap = {
   tick: 'hour',
   min: 'day',
   hour: 'month'
 };
+
+function getEndpointType(timeframe: HistoryConfig['timeframe']): EndpointType {
+  if (timeframe === 'tick') {
+    return 'tick';
+  } else if (timeframe === 'm1' || timeframe === 'm30') {
+    return 'min';
+  } else {
+    return 'hour';
+  }
+}
 
 function generateRequestData(
   symbol: HistoryConfig['symbol'],
