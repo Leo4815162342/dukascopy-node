fc42555ab390b43fa3ff7d568b80e70811fd38c0
Leo4815162342 <leonid.pyrlya@gmail.com>
2019-06-02 19:47:56 +0200

    more h1 tests

--- test-call.ts
+++ test-call.ts
@@ -5,16 +5,16 @@ import { getHistoricRates } from './src';
   try {
     const config: HistoryConfig = {
       symbol: 'eurusd',
-      dates: { start: '2019-02-05 01:00', end: '2019-02-05 02:00' },
-      timeframe: 'm1',
+      dates: { start: '2019-02-05', end: '2019-02-05 12:00' },
+      timeframe: 'h1',
       volumes: true,
-      gmtOffset: 0
+      gmtOffset: -60
     };
     console.log('fetching');
     console.log(JSON.stringify(config));
     const quotes = await getHistoricRates(config);
     console.log(quotes);
-    console.log(quotes.length);
+    console.log(`size: ${quotes.length}`);
     // console.log(quotes[0][0]);
     // console.log(quotes[quotes.length - 1][0]);
   } catch (error) {
