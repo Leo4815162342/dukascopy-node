35d095e7e64fe5ecc20630f4698a97e50095cdba
Leo4815162342 <leonid.pyrlya@gmail.com>
2019-06-05 23:41:35 +0200

    one more d1 test

--- test-call.ts
+++ test-call.ts
@@ -5,15 +5,15 @@ import { getHistoricRates } from './src';
   try {
     const config: HistoryConfig = {
       instrument: 'eurusd',
-      dates: { from: '2019-02-04 23:59', to: '2019-02-05 00:01' },
-      timeframe: 'tick',
+      dates: { from: '2019-01-15', to: '2019-01-22' },
+      timeframe: 'd1',
       volumes: true,
-      utcOffset: 0
+      utcOffset: -1440
     };
     console.log('fetching');
     console.log(JSON.stringify(config));
     const quotes = await getHistoricRates(config);
-    console.log(JSON.stringify(quotes, null, 2));
+    console.log(JSON.stringify(quotes));
     console.log(`size: ${quotes.length}`);
     // console.log(quotes[0][0]);
     // console.log(quotes[quotes.length - 1][0]);
