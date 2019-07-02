9ed7a993d8cc490f5997af4afd58d3edc4f64cec
Leo4815162342 <leonid.pyrlya@gmail.com>
2019-07-02 22:35:16 +0200

    ignore tests form code coverage report

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '.(ts|tsx)': 'ts-jest'
   },
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js']
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  coveragePathIgnorePatterns: ['tests']
 };
