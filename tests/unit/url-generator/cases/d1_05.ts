35df85edf8cc499b571e251270776235876242e9
Leo4815162342 <leonid.pyrlya@gmail.com>
2019-07-25 19:33:34 +0200

    more url generator unit tests

---
+++ tests/unit/url-generator/cases/d1_05.ts
@@ -0,0 +1,11 @@
+const input = {
+  instrument: 'btcusd',
+  timeframe: 'd1',
+  startDate: new Date('2019-07-08T00:00:00.000Z'),
+  endDate: new Date('2019-07-08T22:00:00.000Z'),
+  priceType: 'bid'
+};
+
+const expectedOutput: any[] = [];
+
+export { input, expectedOutput };
