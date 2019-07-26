911e3d5c0e5a0e9bb654f5bfe5ebbe883480a021
Leo4815162342 <leonid.pyrlya@gmail.com>
2019-07-26 18:17:18 +0200

    spy on process function for integration tests

--- tests/integration/index.test.ts
+++ tests/integration/index.test.ts
@@ -4,26 +4,28 @@
 // m1, m30: EURUSD, bid, 2019-02-04 - 2019-02-06
 // h1, d1, mn1: EURUSD, bid, 2019-01-01, 2019-03-01
 
-import { HistoryConfig } from '../../src/config/types';
+import { PromiseType } from 'utility-types';
 
 import { getTestCases, getConfigDescription } from '../utils';
 
 import * as lib from '../../src';
 import * as configValidator from '../../src/config-validator';
+import * as processor from '../../src/processor';
 
 jest.mock('node-fetch', require('../__mocks__/fetch').default);
 
 const getHistoricRatesFn = jest.spyOn(lib, 'getHistoricRates');
 const validateConfigFn = jest.spyOn(configValidator, 'validateConfig');
+const processDataFn = jest.spyOn(processor, 'processData');
 
 type TestCase = {
-  config: HistoryConfig;
-  expectedOutput: number[][];
+  config: Parameters<typeof lib.getHistoricRates>[0];
+  expectedOutput: PromiseType<ReturnType<typeof lib.getHistoricRates>>;
 };
 
 describe('getHistoricRates', () => {
   const testCases = getTestCases<TestCase>('tests/integration/cases');
-  [testCases[0], testCases[1]].forEach(({ content, path }) => {
+  testCases.forEach(({ content, path }) => {
     if (path.indexOf('fail_') >= 0) {
       generateFailTestCase(content);
     } else {
@@ -59,6 +61,10 @@ function generateSuccessTestCase({ config, expectedOutput }: TestCase) {
 
         it('should call "validateConfig" function once', () => {
           expect(validateConfigFn).toHaveBeenCalledTimes(1);
+        });
+
+        it('should call "processData" function once', () => {
+          expect(processDataFn).toHaveBeenCalledTimes(1);
         });
 
         it('should generate correct output', () => {
