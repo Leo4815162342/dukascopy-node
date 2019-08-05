6686619d0dba0f756a61e42bf381a475a603a917
Leo4815162342 <leonid.pyrlya@gmail.com>
2019-08-05 23:06:50 +0200

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transform: {
-    '.(ts|tsx|json)': 'ts-jest'
+    '.(ts|tsx)': 'ts-jest'
   },
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
