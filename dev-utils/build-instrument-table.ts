0cd34bf616ac4940d08f3b220ed6839228274df5
Leo4815162342 <leonid.pyrlya@gmail.com>
2019-08-11 12:56:32 +0200

    Updated instrument table with a breakdown by group

--- dev-utils/build-instrument-table.ts
+++ dev-utils/build-instrument-table.ts
@@ -1,11 +1,42 @@
 import fs from 'fs';
 import { promisify } from 'util';
 
-import { instruments } from '../src/config/instruments';
+import { instruments, Instrument } from '../src/config/instruments';
+
+const groupMap = [
+  { group: 'Forex', subs: ['FX_CROSSES', 'FX_MAJORS', 'FX_METALS'] },
+  { group: 'Commodities', subs: ['CMD_AGRICULTURAL', 'CMD_ENERGY', 'CMD_METALS'] },
+  { group: 'Indicies (CFD)', subs: ['IDX_AMERICA', 'IDX_ASIA', 'IDX_EUROPE'] },
+  { group: 'Bonds (CFD)', subs: ['BND_CFD'] },
+  { group: 'Austria 🇦🇹', subs: ['Austria'] },
+  { group: 'Belgium 🇧🇪', subs: ['Belgium'] },
+  { group: 'Denmark 🇩🇰', subs: ['Denmark'] },
+  { group: 'Finland 🇫🇮', subs: ['Finland'] },
+  { group: 'France 🇫🇷', subs: ['France'] },
+  { group: 'Germany 🇩🇪', subs: ['Germany'] },
+  { group: 'Netherlands 🇳🇱', subs: ['Netherlands'] },
+  { group: 'Norway 🇳🇴', subs: ['Norway'] },
+  { group: 'Portugal 🇵🇹', subs: ['Portugal'] },
+  { group: 'Spain 🇪🇸', subs: ['Spain'] },
+  { group: 'Sweden 🇸🇪', subs: ['Sweden'] },
+  // { group: 'Switzerland 🇨🇭', subs: ['Switzerland'] },
+  { group: 'United Kingdom 🇬🇧', subs: ['UK'] },
+  { group: 'United States 🇺🇸', subs: ['US'] },
+  { group: 'ETF (CFD)', subs: ['ETF_CFD_US'] },
+  { group: 'Crypto', subs: ['VCCY'] }
+];
+
+const groups = Object.entries(instruments).reduce((all, [id, instrument]) => {
+  const idx = groupMap.findIndex(({ subs }) => subs.includes(instrument.group));
+
+  all[idx].list.push({ ...instrument, id });
+
+  return all;
+}, groupMap.map(({ group }) => ({ group, list: [] })));
 
 (async () => {
   try {
-    const titles = [
+    const headers = [
       'Instrument id',
       'Instrument name',
       'Description',
@@ -13,16 +44,22 @@ import { instruments } from '../src/config/instruments';
       'Start date (d1, mn1)'
     ];
 
-    const header = titles.map((h, i) => `${!i ? '|' : ''}${h}|`).join('');
-    const divider = titles.map((_, i) => `${!i ? '|' : ''}-|`).join('');
+    const header = headers.map((h, i) => `${!i ? '|' : ''}${h}|`).join('');
+    const divider = headers.map((_, i) => `${!i ? '|' : ''}-|`).join('');
 
-    const body = Object.entries(instruments)
-      .map(([id, { name, description, minStartDate, minStartDateDaily }]) => {
-        return `|\`${id}\`|${name}|${description}|${minStartDate}|${minStartDateDaily}`;
+    const output = groups
+      .map(({ group, list }) => {
+        const groupTitle = `### ${group}`;
+
+        const listBody = list
+          .map(({ id, name, description, minStartDate, minStartDateDaily }) => {
+            return `|\`${id}\`|${name}|${description}|${minStartDate}|${minStartDateDaily}|`;
+          })
+          .join('\n');
+
+        return [groupTitle, header, divider, listBody].join('\n');
       })
       .join('\n');
-
-    const output = [header, divider, body].join('\n');
 
     await promisify(fs.writeFile)('./INSTRUMENTS.md', output);
     console.log('Created file');
@@ -30,3 +67,31 @@ import { instruments } from '../src/config/instruments';
     console.log(err);
   }
 })();
+
+// (async () => {
+//   try {
+//     const headers = [
+//       'Instrument id',
+//       'Instrument name',
+//       'Description',
+//       'Start date (tick, m1, h1)',
+//       'Start date (d1, mn1)'
+//     ];
+
+//     const header = headers.map((h, i) => `${!i ? '|' : ''}${h}|`).join('');
+//     const divider = headers.map((_, i) => `${!i ? '|' : ''}-|`).join('');
+
+//     const body = Object.entries(instruments)
+//       .map(([id, { name, description, minStartDate, minStartDateDaily }]) => {
+//         return `|\`${id}\`|${name}|${description}|${minStartDate}|${minStartDateDaily}`;
+//       })
+//       .join('\n');
+
+//     const output = [header, divider, body].join('\n');
+
+//     await promisify(fs.writeFile)('./INSTRUMENTS.md', output);
+//     console.log('Created file');
+//   } catch (err) {
+//     console.log(err);
+//   }
+// })();
---
+++ dev-utils/country-map.ts
@@ -0,0 +1,16 @@
+const patterns = {
+  '^[A-Z]{3}/[A-Z]{3}$': 'Forex',
+  '^.+.CMD/.+': 'Commodities',
+  '^.+.IDX/.+': 'Indicies (CFD)',
+  '^.+.TR/.+': 'Bonds (CFD)',
+  '^.+.AT/.+': 'Austria',
+  '^.+.DK/.+': 'Denmark',
+  '^.+.FI/.+': 'Finland',
+  '^.+.DE/.+': 'Germany',
+  '^.+.NL/.+': 'Netherlands',
+  '^.+.NO/.+': 'Norway',
+  '^.+.PT/.+': 'Portugal',
+  '^.+.ES/.+': 'Spain',
+  '^.+.SE/.+': 'Sweden',
+  '^.+.CH/.+': 'Switzerland'
+};
