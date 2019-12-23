4ee2e578e10b6d7bd8b0e3e48254772017962c7d
Leo4815162342 <leonid.pyrlya@gmail.com>
2019-12-23 12:30:17 +0100

    validation schema type

--- packages/dukascopy-core/src/config-validator/schema.ts
+++ packages/dukascopy-core/src/config-validator/schema.ts
@@ -1,8 +1,9 @@
+import { ValidationSchema } from 'fastest-validator';
 import { instruments } from '../config/instruments';
 import { timeframes } from '../config/timeframes';
 import { priceTypes } from '../config/price-types';
 
-const schema = {
+const schema: ValidationSchema = {
   instrument: { type: 'string', enum: Object.keys(instruments), required: true },
   dates: {
     type: 'object',
