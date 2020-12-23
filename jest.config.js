10fafd8e1888aad1053c327f9ac389cde4ab836c
Leo4815162342 <leonid.pyrlya@gmail.com>
2020-12-23 19:10:14 +0100

    (build): respect root config and ignore files for prettier and eslint

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '.(ts|js)': 'ts-jest'
-  },
-  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|js)$',
-  moduleFileExtensions: ['ts', 'js'],
-  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|bin|lib|build)[/\\\\]'],
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|ts)$']
-};
