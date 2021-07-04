d5f9b8736bedfe3540cfcbefcbc961473d8837d1
Leo4815162342 <leonid.pyrlya@gmail.com>
2021-07-04 22:44:40 +0200

    fix: tests

--- packages/dukascopy-node/src/dates-normaliser/tests/cases/h1_01.ts
+++ packages/dukascopy-node/src/dates-normaliser/tests/cases/h1_01.ts
@@ -5,6 +5,6 @@ const input = {
   timeframe: 'h1'
 };
 
-const expectedOutput = [new Date('2003-05-04T19:00:00.000Z'), new Date('2019-07-12T21:05:00.000Z')];
+const expectedOutput = [new Date('2003-05-04T21:00:00.000Z'), new Date('2019-07-12T21:05:00.000Z')];
 
 export { input, expectedOutput };
