c592a057a987774c27597506fcded22e130044a1
Leo4815162342 <leonid.pyrlya@gmail.com>
2021-08-16 22:39:25 +0200

    feat: updated schema

--- packages/dukascopy-node/src/config-validator/schema.ts
+++ packages/dukascopy-node/src/config-validator/schema.ts
@@ -11,7 +11,7 @@ import { Timeframe } from '../config/timeframes';
 import { Price } from '../config/price-types';
 import { Format } from '../config/format';
 
-import { Config } from '../config';
+import { defaultConfig, Config } from '../config';
 
 export type InputSchema<Input> = Record<keyof Required<Input>, ValidationRule>;
 
@@ -29,30 +29,60 @@ export const schema: InputSchema<Config> = {
     type: 'string',
     enum: Object.keys(Timeframe),
     optional: true,
-    default: Timeframe.d1
+    default: defaultConfig.timeframe
   } as RuleString,
   priceType: {
     type: 'string',
     enum: Object.keys(Price),
     optional: true,
-    default: Price.bid
+    default: defaultConfig.priceType
   } as RuleString,
-  utcOffset: { type: 'number', integer: true, optional: true, default: 0 } as RuleNumber,
-  volumes: { type: 'boolean', optional: true, default: true } as RuleBoolean,
-  ignoreFlats: { type: 'boolean', optional: true, default: true } as RuleBoolean,
+  utcOffset: {
+    type: 'number',
+    integer: true,
+    optional: true,
+    default: defaultConfig.utcOffset
+  } as RuleNumber,
+  volumes: { type: 'boolean', optional: true, default: defaultConfig.volumes } as RuleBoolean,
+  ignoreFlats: {
+    type: 'boolean',
+    optional: true,
+    default: defaultConfig.ignoreFlats
+  } as RuleBoolean,
   format: {
     type: 'string',
     enum: Object.keys(Format),
     optional: true,
-    default: Format.array
+    default: defaultConfig.format
   } as RuleString,
-  batchSize: { type: 'number', integer: true, optional: true, default: 10 } as RuleNumber,
+  batchSize: {
+    type: 'number',
+    integer: true,
+    optional: true,
+    default: defaultConfig.batchSize
+  } as RuleNumber,
   pauseBetweenBatchesMs: {
     type: 'number',
     integer: true,
     optional: true,
-    default: 10
+    default: defaultConfig.pauseBetweenBatchesMs
   } as RuleNumber,
-  useCache: { type: 'boolean', optional: true, default: false } as RuleBoolean,
-  cacheFolderPath: { type: 'string', optional: true, default: '' } as RuleString
+  useCache: { type: 'boolean', optional: true, default: defaultConfig.useCache } as RuleBoolean,
+  cacheFolderPath: {
+    type: 'string',
+    optional: true,
+    default: defaultConfig.cacheFolderPath
+  } as RuleString,
+  onFetchFailRetries: {
+    type: 'number',
+    integer: true,
+    optional: true,
+    default: defaultConfig.onFetchFailRetries
+  } as RuleNumber,
+  waitAfterOnFetchFailRetry: {
+    type: 'number',
+    integer: true,
+    optional: true,
+    default: defaultConfig.pauseBetweenBatchesMs
+  } as RuleNumber
 };
