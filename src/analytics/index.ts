bb86d498e5e8fb2210b5be1e26d36e3a00b41ada
Leo4815162342 <leonid.pyrlya@gmail.com>
2022-05-02 17:46:58 +0200

    feat: analytics update

--- src/analytics/index.ts
+++ src/analytics/index.ts
@@ -1,4 +1,6 @@
+import os from 'os';
 import fetch from 'node-fetch';
+import { version } from '../../package.json';
 import { Config } from '../config';
 
 export enum GOALS {
@@ -11,13 +13,10 @@ export function trackCustomGoal(
   isValid: boolean,
   source: 'cli' | 'node'
 ) {
-  let timezone = '';
-
-  try {
-    timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  } catch (err) {
-    timezone = '';
-  }
+  const timezone = getSafeVal(() => Intl.DateTimeFormat().resolvedOptions().timeZone);
+  const os_type = getSafeVal(() => os.type());
+  const os_release = getSafeVal(() => os.release());
+  const os_platform = getSafeVal(() => os.platform());
 
   try {
     fetch('https://plausible.io/api/event', {
@@ -28,6 +27,7 @@ export function trackCustomGoal(
         domain: 'dukascopy-node',
         screen_width: 1000,
         props: {
+          lib_version: version,
           is_valid: isValid,
           instrument: config.instrument,
           timeframe: config.timeframe,
@@ -35,7 +35,10 @@ export function trackCustomGoal(
           to_date: config.dates.to,
           source,
           node_version: process.version,
-          timezone
+          timezone,
+          os_type,
+          os_release,
+          os_platform
         }
       }),
       headers: { 'X-Forwarded-For': '127.0.0.1', 'Content-Type': 'application/json' }
@@ -44,4 +47,16 @@ export function trackCustomGoal(
   } catch (err) {}
 
   return true;
+}
+
+function getSafeVal(func: () => string): string {
+  let val = '';
+
+  try {
+    val = func();
+  } catch (err) {
+    val = '';
+  }
+
+  return val;
 }
