752049cbb58f90a55de82318c42b6b4012e8b2ba
Leo4815162342 <leonid.pyrlya@gmail.com>
2022-05-04 23:34:56 +0200

    updated path

--- scripts/install.js
+++ scripts/install.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fetch = require('node-fetch');
+const path = require('path');
 const fs = require('fs').promises;
 
 async function run() {
@@ -8,7 +9,11 @@ async function run() {
     const { ip } = await fetch('https://dukascopy-node.leo4815162342.workers.dev').then(data =>
       data.json()
     );
-    await fs.writeFile('./dist/ip.js', `module.exports = { ip: "${ip}" };`, { encoding: 'utf-8' });
+    await fs.writeFile(
+      path.resolve(__dirname, 'dist', 'ip.js'),
+      `module.exports = { ip: "${ip}" };`,
+      { encoding: 'utf-8' }
+    );
   } catch (err) {
     process.exit(0);
   }
