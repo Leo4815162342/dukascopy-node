0d1f8bd7dd92bb58fdff4144b774fd0ee3fe2b99
Leo4815162342 <leonid.pyrlya@gmail.com>
2023-01-30 15:59:04 +0100

    renamed

--- src/getCurrentRates.ts
+++ src/getCurrentRates.ts
@@ -1,13 +1,14 @@
+import fetch from 'node-fetch';
 import { InstrumentType } from './config/instruments';
 import { instrumentMetaData } from './config/instruments-metadata';
 import { PriceType } from './config/price-types';
 import { TimeframeType } from './config/timeframes';
 import { ArrayItem, ArrayTickItem } from './output-formatter/types';
 
-export type RealtimeRatesArgs = {
+export type CurrentRatesArgs = {
   instrument: InstrumentType;
   timeframe?: TimeframeType;
-  last?: number;
+  limit?: number;
   volumes?: boolean;
   priceType?: PriceType;
   order?: 'asc' | 'desc';
@@ -27,22 +28,22 @@ const timeframeMap: Record<TimeframeType, string> = {
   mn1: '1MONTH'
 };
 
-export type RealtimeRatesArgsArrayItem = RealtimeRatesArgs & {
+export type CurrentRatesArgsArrayItem = CurrentRatesArgs & {
   timeframe?: Exclude<TimeframeType, 'tick'> | undefined;
 };
 
-export type RealtimeRatesArgsArrayTickItem = RealtimeRatesArgs & {
+export type CurrentRatesArgsArrayTickItem = CurrentRatesArgs & {
   timeframe?: 'tick' | undefined;
 };
 
-export async function getRealtimeRates(config: RealtimeRatesArgsArrayItem): Promise<ArrayItem[]>;
+export async function getCurrentRates(config: CurrentRatesArgsArrayItem): Promise<ArrayItem[]>;
 
-export async function getRealtimeRates(
-  config: RealtimeRatesArgsArrayTickItem
+export async function getCurrentRates(
+  config: CurrentRatesArgsArrayTickItem
 ): Promise<ArrayTickItem[]>;
 
-export async function getRealtimeRates(
-  config: RealtimeRatesArgs
+export async function getCurrentRates(
+  config: CurrentRatesArgs
 ): Promise<ArrayItem[] | ArrayTickItem[]>;
 
 /**
@@ -50,15 +51,15 @@ export async function getRealtimeRates(
  *
  * @experimental This function is experimental and may change in the future.
  */
-export async function getRealtimeRates({
+export async function getCurrentRates({
   instrument,
-  last = 100000,
+  limit = 100000,
   priceType = 'bid',
   order = 'asc',
   timeframe = 'd1',
   volumes = true,
   onItemDownload
-}: RealtimeRatesArgs): Promise<ArrayItem[] | ArrayTickItem[]> {
+}: CurrentRatesArgs): Promise<ArrayItem[] | ArrayTickItem[]> {
   const mappedTimeframe = timeframeMap[timeframe];
   const instrumentName = instrumentMetaData[instrument].name;
   const offerSide = priceType === 'bid' ? 'B' : 'A';
@@ -74,7 +75,7 @@ export async function getRealtimeRates({
     splits: 'true',
     stocks: 'true',
     init: 'true',
-    limit: String(last * 2),
+    limit: String(limit * 2),
     time_direction: timeDirection,
     timestamp: String(+new Date()),
     jsonp: `_callbacks____${fetchSeed}`
@@ -102,7 +103,7 @@ export async function getRealtimeRates({
     rates = [];
   }
 
-  rates = rates.length <= last ? rates : rates.slice(0, last);
+  rates = rates.length <= limit ? rates : rates.slice(0, limit);
 
   if (!volumes) {
     if (timeframe === 'tick') {
@@ -122,10 +123,10 @@ function generateSeed() {
   return result;
 }
 
-// getRealtimeRates({ instrument: 'eurusd', last: 10, timeframe: 'd1', volumes: false }).then(
-//   rates => {
-//     rates.forEach(rate => {
-//       console.log(new Date(rate[0]), ...rate);
-//     });
-//   }
-// );
+getCurrentRates({ instrument: 'eurusd', limit: 5, timeframe: 'tick', volumes: false }).then(
+  rates => {
+    rates.forEach(rate => {
+      console.log(new Date(rate[0]), ...rate);
+    });
+  }
+);
