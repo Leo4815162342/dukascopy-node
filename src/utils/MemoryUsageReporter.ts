b0d7e039ced2d5f5a9a78e9f729275eb93e178de
Leo4815162342 <leonid.pyrlya@gmail.com>
2023-02-15 13:46:37 +0100

    misc: memory usage reporter

---
+++ src/utils/MemoryUsageReporter.ts
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+import { createWriteStream, WriteStream } from 'fs';
+
+export class MemoryUsageReporter {
+  interval: NodeJS.Timeout | null;
+  intervalStep: number;
+  writeStream: WriteStream;
+
+  constructor(csvReportPath: string, intervalStep = 1000) {
+    this.interval = null;
+    this.intervalStep = intervalStep;
+    this.writeStream = createWriteStream(csvReportPath, {
+      flags: 'a+'
+    });
+
+    process
+      .on('SIGINT', () => this.stop('SIGINT'))
+      .on('SIGTERM', () => this.stop('SIGTERM'))
+      .on('unhandledRejection', () => this.stop('unhandledRejection'));
+  }
+
+  start() {
+    console.log('Starting memory usage tracker');
+    this.interval = setInterval(() => {
+      const { rss, heapTotal, heapUsed, external, arrayBuffers } = process.memoryUsage();
+
+      this.writeStream.write(
+        [rss, heapTotal, heapUsed, external, arrayBuffers]
+          .map(num => (num / 1024 / 1024).toFixed(2))
+          .join(',') + '\n'
+      );
+    }, this.intervalStep);
+  }
+
+  stop(reason: string) {
+    if (!this.interval) {
+      console.log('Tried to stop memory usage tracker, but it was already stopped');
+      return;
+    }
+    console.log('Stopping memory usage tracker', reason);
+    clearInterval(this.interval);
+    this.interval = null;
+    process.exit(0);
+  }
+}
